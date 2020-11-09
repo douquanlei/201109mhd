a5a97ea95e8c38ab3cde0b4c2e27f723a38f2b1a
2580977936@qq.com <2580977936@qq.com>
2020-11-09 21:08:43 +0800

    1109

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/Home'
+import Classify from '../views/Classify'
+import Hello from '../views/Hello'
+import Favorite from '../views/Hello/Favorite'
+import History from '../views/Hello/History'
+import Login from '../views/Login'
+import My from '../views/My'
+import Ranking from '../views/Ranking'
+import Resigter from '../views/Resigter'
+import SearchResult from '../views/SearchResult'
+import Vip from '../views/Vip'
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/home', component: Home },
+    { path: '/classify', component: Classify },
+    {
+      path: '/hello',
+      component: Hello,
+      children: [
+        { path: 'history', component: History },
+        { path: 'favorite', component: Favorite }
+      ]
+    },
+    { path: '/favorite', component: Favorite },
+    { path: '/history', component: History },
+    { path: '/login', component: Login },
+    { path: '/my', component: My },
+    { path: '/ranking', component: Ranking },
+    { path: '/resigter', component: Resigter },
+    { path: '/search-result', component: SearchResult },
+    { path: '/vip', component: Vip },
+    { path: '/', redirect: Home }
+  ]
 })
-
 export default router
